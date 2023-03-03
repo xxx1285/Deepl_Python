984f23da9cb158810cf744e3f7e59c37a325b501
Alex18 <00018ds@gmail.com>
2023-03-03 23:46:48 +0200

    modified:   Game_SQL_Import_export/game_mysqldump-GOOD4.py 	modified:   SQL_clon_modx_resourse/python_sql_modx_resourse.py 	new file:   SQL_clon_modx_resourse/test_date.py

--- SQL_clon_modx_resourse/python_sql_modx_resourse.py
+++ SQL_clon_modx_resourse/python_sql_modx_resourse.py
@@ -1,192 +1,174 @@
-
-
 # import os
+import time
+import random
 import re
 import json
 import pymysql
+from bs4 import BeautifulSoup
+import transliterate
+import datetime
+import deepl
 
+
+# функція що очищає або добавляє екранування - JSON
+def my_clean_string(string, escape=True):
+    if escape:
+        # Замінюємо спеціальні символи на їх екрановані еквіваленти
+        string = string.replace('/', '\/')
+    else:
+        # Видаляємо екранування спеціальних символів
+        string = string.replace('\\', '')
+        string = string.replace('\n', '')
+        string = string.replace('\t', '')
+        string = string.replace('&nbsp;', '')
+    return string
+
+xren = 0
 
 # SQL Зчитуємо параметри з конфігураційного файлу
-with open(r'SQL_clon_modx_resourse\configs\config_sql_modx_resouses.json') as f:
+with open(r'SQL_clon_modx_resourse\configs\config_sql_Deepl.json') as f:
     config = json.load(f)
 
-# Експорт таблиць
-my_tables = ['modx_site_content']
 
+"""
+    modx_context_setting                настройки контекстов (catalog id, site_start, site_url, nazva)
+    modx_site_content                   ресурсы сайта
+    modx_site_tmplvar_contentvalues     VNIMANIE - BABEL
+    modx_games_co                       MIGX content Resourses
+"""
+my_tables = ['modx_context_setting', 'modx_site_content', 'modx_site_tmplvar_contentvalues',
+             'modx_games_co']
+# my_tables_str = ", ".join(my_tables)
+
+# Контексти з якого копіюємо
+context_web = 'web'
 # Контексти та мови для перекладу
 context_and_lang = ["ru","es","pl","pt","fr","id","el","de","tr","hu","uk","it","ro","bg","fi","et","lv","nl"]
 
-# modx_context_setting - настройки контекстов - проставить catalog id, site_start, site_url, nazva
-# modx_site_content - ресурсы сайта
-# modx_site_tmplvar_contentvalues ------- VNIMANIE - BABEL
+# Поля що потрібно перекласти
+translate_name = ['pagetitle','longtitle','description','menutitle']
 
-
-
-# my_tables_str = ", ".join(my_tables)
-
-# Функция для удаления экранирования лапок
-def remove_quotes(s):
-    if s[0] == "'" and s[-1] == "'":
-        return s[1:-1].replace("\\'", "'")
-    return s
+standart_nalashtuvannya = {
+    'published': 0,
+    'editedby': 0,
+    'editedon': 0,
+    'publishedon': 0,
+    'publishedby': 0
+}
+# дата UNIX на даний час
+new_create_date = {
+    'createdon': int(time.time()),
+    'pub_date': int(time.time())
+}
 
 
 try:
     # Приєднуємось до бази для Export SQL
-    connect_export_1 = pymysql.connect(host=config['export_1']['host'],
+    connect_database = pymysql.connect(**config['config_database'],
                                        port=3306,
-                                       user=config['export_1']['user'],
-                                       password=config['export_1']['password'],
-                                       database=config['export_1']['database'],
                                        cursorclass=pymysql.cursors.DictCursor
                                        )
-    print("succesfully connect Export base..")
+    print("\n*************************************\
+            \n--> Succesfully connect - " + config['config_database']['database'])
 
-    # Відкриваємо файл для запису SQL-дампу
-    with open(output_file, 'w', encoding='utf8') as f:
-        # Проходимося по всім таблицям і отримуємо їх структуру та дані
-        for table in my_tables:
-            """
-            Використання with забезпечує правильне закриття з'єднання з базою даних, навіть у випадку,
-            якщо сталася помилка в середині блоку with. Це дає можливість уникнути потенційних
-            проблем з пам'яттю та забезпечити коректну роботу з базою даних.
-            Використовуючи with конструкцію, ми можемо гарантувати, що після завершення
-            виконання програми об'єкти, які були відкриті, будуть автоматично закриті.
-            Це дуже зручно, оскільки нам не потрібно додатково вказувати закриття курсора
-            та з'єднання з базою даних.
-            Курсор cursor() викор... для виконання SQL запитів та отримання резу.
-            """
-            with connect_export_1.cursor() as cursor:
-                # Отримуємо структуру таблиці
-                cursor.execute(f"SHOW CREATE TABLE {table}")
-                create_table_query = cursor.fetchone()['Create Table']
-                f.write(f"{create_table_query};\n")
+    # Ініціалізуємо Deepl API key
+    translator = deepl.Translator(config['config_deepl']['deepl_api_key'])
+    print("--> Deepl API - OK \n*************************************")
 
-                # Експортуємо таблиці у SQL форматі
-                sql_export = f"SELECT * FROM {table}"
-                cursor.execute(sql_export)
-                rows = cursor.fetchall()
+    with connect_database.cursor() as my_cursor:
+        # Выбор всех строк таблицы modx_site_content, где context = 'web'
+        my_cursor.execute(f"SELECT * FROM modx_site_content WHERE `context_key` = '{context_web}' \
+                            AND (`id` = 75 OR `template` IN (6,7,19))")
+        rows_database = my_cursor.fetchall()
 
-                # Загальний список значень для таблиці
-                all_values = []
+        # перебираємо контексти - lang
+        for lang in context_and_lang:
+            # перебираємо кожен вибраний рядок в таблиці
+            for row in rows_database:
+                # Клонуємо рядок
+                new_row = row.copy()
 
-                # перебираємо кожен рядок таблиці
-                for row in rows:
-                    # values = ', '.join([f"'{value.hex()}'" if isinstance(value, bytes) else f"'{value}'" for value in row.values()])
-                    # формируємо список значень для кожного рядка
-                    values = []
-                    for value in row.values():
-                        # Якщо значення є NULL, додати до списку значення 'NULL'
-                        if value is None:
-                            values.append('NULL')
-                        elif isinstance(value, bytes):
-                            value = value.hex()
-                            values.append(f"'{value}'")
-                        # Якщо значення є рядком, додати до списку лапки з екрануванням лапок всередині рядка
-                        elif isinstance(value, str):
-                            # values.append("'" + value.replace("'", "\\'") + "'")
-                            # value = value.replace("'", "\\'").replace('"', '\\"')
-                            value = value.replace("'", "\\'")
-                            values.append(f"'{value}'")
-                        # Якщо значення є числом, додати до списку без лапок
-                        elif isinstance(value, (int, float)):
-                            values.append(f"{value}")
-                        else:
-                            values.append(f"'{value}'")
-                        # добавляем значение в список значений
+                # Змінюємо значення ключа context на поточне значення мови
+                new_row['context_key'] = lang
 
-                    # объединяем список значений в строку, разделяя их запятыми
-                    values = ', '.join(values)
+                # randon секунди дати UNIX
+                random_time = random.choice([15150, 18300, 21500, 23700, 40320])
 
-                    # об'єднуємо список полів до таблиці
-                    fields = ', '.join([f"`{key}`" for key in row.keys()])
+                new_create_date['createdon'] = new_create_date['createdon'] + random_time
+                new_row['createdon'] = new_create_date['createdon']
 
-                    # Добаляємо значення в Загальний список значень для таблиці
-                    all_values.append(f"({values})")
+                new_create_date['pub_date'] = new_create_date['pub_date'] + random_time
+                new_row['pub_date'] = new_create_date['pub_date']
 
-                # об'єднуємо загальий список значень таблиці розділяючи комами та з нової
-                all_values = ', \n'.join(all_values)
+                # Перекладаємо поля використовуючи DEEPL
+                for name in translate_name:
+                    new_row[name] = translator.translate_text(new_row[name], target_lang=lang)
+                    print(new_row[name])
 
-                # fields = ', '.join([f"`{key}`" for key in row.keys()])
-                #     # insert_query = f"INSERT INTO `{table}` ({fields}) VALUES ({values});\n"
-            insert_query = f"INSERT INTO `{table}` ({fields}) VALUES\n{all_values};\n"
 
-            # додаємо вираз вставки даних до файлу
-            f.write(insert_query)
 
-            # додаємо символ переведення рядка до файлу, щоб розділити таблиці
-            f.write('\n')
+                xren += 1
+                print(str(xren) + ' - ' +str(new_row['pub_date']) + ' - ' + str(datetime.datetime.fromtimestamp(new_row['pub_date'])))
+                # print(datetime.datetime.fromtimestamp(new_row['pub_date']))
+
+                # for key, value in standart_nalashtuvannya.items():
+                #     new_row[key] = value
+                #     print(new_row)
+                #     print(new_row)
+                # print(new_row)
+                # print(new_row)
+                # {**new_row, **{key: value for key, value in standart_nalashtuvannya.items()}}
+
+
+
+        #     # Загальний список значень для таблиці
+        #     all_values = []
+
+        #     # перебираємо кожен рядок таблиці
+        #     for row in rows:
+        #         # values = ', '.join([f"'{value.hex()}'" if isinstance(value, bytes) else f"'{value}'" for value in row.values()])
+        #         # формируємо список значень для кожного рядка
+        #         values = []
+        #         for value in row.values():
+        #             # Якщо значення є NULL, додати до списку значення 'NULL'
+        #             if value is None:
+        #                 values.append('NULL')
+        #             elif isinstance(value, bytes):
+        #                 value = value.hex()
+        #                 values.append(f"'{value}'")
+        #             # Якщо значення є рядком, додати до списку лапки з екрануванням лапок всередині рядка
+        #             elif isinstance(value, str):
+        #                 # values.append("'" + value.replace("'", "\\'") + "'")
+        #                 # value = value.replace("'", "\\'").replace('"', '\\"')
+        #                 value = value.replace("'", "\\'")
+        #                 values.append(f"'{value}'")
+        #             # Якщо значення є числом, додати до списку без лапок
+        #             elif isinstance(value, (int, float)):
+        #                 values.append(f"{value}")
+        #             else:
+        #                 values.append(f"'{value}'")
+        #             # добавляем значение в список значений
+
+        #         # объединяем список значений в строку, разделяя их запятыми
+        #         values = ', '.join(values)
+
+        #         # об'єднуємо список полів до таблиці
+        #         fields = ', '.join([f"`{key}`" for key in row.keys()])
+
+        #         # Добаляємо значення в Загальний список значень для таблиці
+        #         all_values.append(f"({values})")
+
+        #     # об'єднуємо загальий список значень таблиці розділяючи комами та з нової
+        #     all_values = ', \n'.join(all_values)
+
+        #     # fields = ', '.join([f"`{key}`" for key in row.keys()])
+        #     #     # insert_query = f"INSERT INTO `{table}` ({fields}) VALUES ({values});\n"
+        # # insert_query = f"INSERT INTO `{table}` ({fields}) VALUES\n{all_values};\n"
+
 
 except Exception as ex:
     print("Connection refused...")
     print(ex)
 finally:
-    connect_export_1.close()
+    connect_database.close()
     print("Connection Export CLOSE")
-
-# Import SQL
-try:
-    # Приєднуємось до бази для Import SQL
-    connect_import_2 = pymysql.connect(**config['import_2'],
-                                        port=3306,
-                                        cursorclass=pymysql.cursors.DictCursor
-                                        )
-    # sql_mode='NONE',  # режим совместимости SQL
-    print("succesfully connect Import base..")
-
-    # Використання with забезпечує правильне закриття з'єднання з базою даних
-    with connect_import_2.cursor() as cursor:
-
-        # Перебираємо та видаляємо таблиці з Бази данних
-        for table in my_tables:
-            cursor.execute(f"DROP TABLE IF EXISTS {table};")
-
-        # імпортуємо SQL Dump файл до бази даних
-        # with open(output_file, 'r', encoding='utf-8') as f:
-        #     sql = f.read()
-        #     cursor.execute(sql)
-
-        # Відкриття файлу для читання
-        with open(output_file, 'r', encoding='utf8') as f:
-            # Читання SQL-запитів з файлу
-            sql_import = f.read()
-
-            # Видаляємо екранування лапок
-            # sql_import = re.sub(r"\\'", "'", sql_import)
-
-            # Розділяємо SQL-запити за допомогою ";"
-            # queries = sql_import.split(';')
-
-            # Разделение файла на отдельные SQL-запросы
-            queries = re.findall(r';\n.*?(?=--|$)', sql_import, re.DOTALL)
-
-            for query in queries:
-                # Якщо запит не є порожнім рядком
-                if query.strip():
-                    try:
-                        # Удаление экранирования лапок
-                        query = re.sub(r"'(.*?)'", lambda x: remove_quotes(x.group()), query)
-                        cursor.execute(query)
-                        connect_import_2.commit()
-                    except pymysql.Error as e:
-                        # Якщо виникає помилка "Таблиця вже існує"
-                        if e.args[0] == 1050:
-                            print(f"Таблиця {query.split()[2]} вже існує. Доповнюємо таблицю.")
-                            # Доповнюємо таблицю
-                            cursor.execute(re.sub(r"CREATE TABLE", "INSERT INTO", query))
-                            connect_import_2.commit()
-                        else:
-                            print(f"Помилка: {e}")
-                            connect_import_2.rollback()
-
-            # # Виконання кожного SQL-запиту
-            # for command in sql_import:
-            #     cursor.execute(command)
-
-
-except Exception as ex:
-    print("Connection refused...")
-    print(ex)
-finally:
-    connect_import_2.close()
-    print("Connection import CLOSE")
---
+++ SQL_clon_modx_resourse/test_date.py
@@ -0,0 +1,26 @@
+import datetime
+import time
+import random
+
+timestamp = 1678208220
+timestamp1 = 1678208220 + 15150
+timestamp2 = 1678208220 + 21500 # 5000
+timestamp3 = 1678208220 + 40320 # 5000
+random_number = random.choice([15150, 21500, 40320])
+date = datetime.datetime.fromtimestamp(timestamp)
+date1 = datetime.datetime.fromtimestamp(timestamp1)
+date2 = datetime.datetime.fromtimestamp(timestamp2)
+date3 = datetime.datetime.fromtimestamp(timestamp3)
+
+unix_now_time = int(time.time())
+
+print(date)
+print(str(date1) + ' +6000')
+print(str(date2) + ' +12000')
+print(date3)
+
+# result = 1678208220 + random_number
+# print(datetime.datetime.fromtimestamp(result))
+
+aaaa = int(time.time()) + random.choice([15150, 21500, 40320])
+print(aaaa)
