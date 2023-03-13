5197c29a2a030b6f69f9d0798f7f041681a5d071
Alex18 <00018ds@gmail.com>
2023-03-13 23:24:01 +0200

    modified:   SQL_clon_modx_resourse/generator_sql_modx_resourse.py 	modified:   SQL_clon_modx_resourse/python_sql_modx_resourse.py 	modified:   SQL_clon_modx_resourse/tempCodeRunnerFile.py

--- SQL_clon_modx_resourse/tempCodeRunnerFile.py
+++ SQL_clon_modx_resourse/tempCodeRunnerFile.py
@@ -1,5 +1,8 @@
-        # with connect_database.cursor() as cursor:
-        #     sql_resurs = f"INSERT INTO `modx_site_tmplvar_contentvalues` (tmplvarid, contentid, value) VALUES ({baza_value_new_row})\
-        #                 ON DUPLICATE KEY UPDATE {baza_key_new_row}"
-        #     cursor.execute(sql_resurs)
-        # connect_database.commit()
+            for name in translate_name:
+                if len(new_row[name]) > 0:
+                    # Замінюємо входження зі списку dont_translate на тег <keep>
+                    for word in dont_translate:
+                        new_row[name] = new_row[name].replace(word, f"<keep>{word}</keep>")
+                    translate = translator.translate_text(new_row[name], tag_handling='xml', ignore_tags='keep', target_lang=lang)
+                    new_row[name] = translate.text
+                    new_row[name] = new_row[name].replace("<keep>", "").replace("</keep>", "")
